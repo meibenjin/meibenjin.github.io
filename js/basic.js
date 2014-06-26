070ada49ed4b4b7665d8a80ebd7b0d10b86a1948
meibenjin <meibenjin@gmail.com>
2014-06-26 13:05:48 +0800

    update index.html

---
+++ js/basic.js
@@ -0,0 +1,37 @@
+function load_disqus() {
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'blogmbj';
+    //var disqus_identifier = 'newid1';
+    var disqus_url = 'http://www.meibenjin.cn/index.html';
+    var disqus_config = function () { 
+        this.language = "zh_cn";
+    };
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+         var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+         dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+     })();
+
+    /* * * Disqus Reset Function * * */
+    var reset = function (newIdentifier, newUrl, newTitle, newLanguage) {
+        DISQUS.reset({
+            reload: true,
+            config: function () {
+                this.page.identifier = newIdentifier;
+                this.page.url = newUrl;
+                this.page.title = newTitle;
+                this.language = newLanguage;
+            }
+        });
+    };
+}
+function scroll_top() { 
+    if(document.documentElement.scrollTop==0) {
+        clearTimeout(scrolldelay);
+    }
+    window.scrollBy(0,-10); 
+    scrolldelay = setTimeout('scroll_top()',100); 
+}
+ 
