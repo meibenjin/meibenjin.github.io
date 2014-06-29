d5679224f9dd9d168b6914151697b3e5ecb5ef91
meibenjin <meibenjin@gmail.com>
2014-06-29 16:06:52 +0800

    hide home

--- js/angular-control.js
+++ js/angular-control.js
@@ -42,7 +42,7 @@ function toggle_disqus(value) {
   app = angular.module('blog', ['ngSanitize']).config([
       '$routeProvider', "$locationProvider", function($routeProvider, $locationProvider) {
         return $routeProvider.when("", {
-          templateUrl:"partials/home.html"
+          templateUrl:"partials/posts_list.html"
         }).when("/Home", {
           templateUrl: "partials/home.html"
         }).when("/Blog", {
@@ -94,7 +94,7 @@ function toggle_disqus(value) {
           if (items.length > 1 ) {
             return items[items.length - 1];
           }
-          return "Home";
+          return "Blog";
         };
         $scope.state = getState($location.path());
         $scope.config = data;
